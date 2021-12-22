f8e2d76789c8cf01c472f738fd7d2d4aaa2a5f76
오낙지 <82641608+dhtjddls@users.noreply.github.com>
2021-12-22 18:10:24 +0900

    no message

--- 연습장.py
+++ 연습장.py
@@ -1,15 +1,1 @@
-def make_one(x: int) -> int:
-    dp = [0 for _ in range(x + 1)]
-    
-    for i in range(2, x+1):
-        dp[i] = dp[i-1] + 1
-
-        if i % 2 == 0:
-            dp[i] = min(dp[i], dp[i//2] + 1)
-        if i % 3 == 0:
-            dp[i] = min(dp[i], dp[i//3] + 1)
-    
-    return dp[x]
-
-x = int(input())
-print(make_one(x))
+print([i for i in range(1, 6)])
