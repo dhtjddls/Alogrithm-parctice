84809f090bdb0f5060c7fc052b35b7ef4ea5deaa
오낙지 <82641608+dhtjddls@users.noreply.github.com>
2022-01-06 22:38:55 +0900

    01.06 Alogrithm problem solve update

---
+++ 1927.py
@@ -0,0 +1,13 @@
+import heapq, sys
+input = sys.stdin.readline
+heap = []
+n = int(input())
+for i in range(n):
+    val = int(input())
+    if val == 0:
+        try:
+            print(heapq.heappop(heap))  
+        except:
+            print('0')  
+    else:
+        heapq.heappush(heap, val)
