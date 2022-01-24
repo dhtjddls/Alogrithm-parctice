d52621f8831eeb7f0115931f8102d10bef09ad25
dhtjddls <dhtjddls12@naver.com>
2022-01-25 01:49:54 +0900

    01.24 Alogrithm problem solve update

---
+++ 2156.py
@@ -0,0 +1,13 @@
+def max_wine(n: int) -> int:
+    wine = [0] + [int(input()) for _ in range(n)] + [0]
+    dp = [0] * (n + 2)
+    dp[1] = wine[1]
+    dp[2] = wine[1] + wine[2]
+
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-3]+wine[i-1]+wine[i], dp[i-2]+wine[i], dp[i-1])
+
+    return(dp[n])
+
+n = int(input())
+print(max_wine(n))
