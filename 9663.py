6188d94421d0392585d2f6aea204e6957e969e31
dhtjddls <dhtjddls12@naver.com>
2022-01-26 00:07:46 +0900

    01.25 Alogrithm problem solve update

--- 9663.py
+++ 9663.py
@@ -1,7 +1,28 @@
-n = int(input())
+import sys 
+n = int(sys.stdin.readline()) 
+board = [0] * n 
+ans_count = 0 
+visited = [False] * n 
 
-chess_board = [[0]* n for i in range[n]]
+def check(x): 
+    for i in range(x): 
+        if abs(board[x] - board[i]) == x - i: 
+            return False 
+    return True 
+def n_queen(x): 
+    global ans_count 
+    if x == n: 
+        ans_count += 1 
+        return 
+    for i in range(n): 
+        if visited[i]:
+            continue 
+        board[x] = i 
+        if check(x): 
+            visited[i] = True 
+            n_queen(x + 1) 
+            visited[i] = False 
+            
+n_queen(0) 
+print(ans_count)
 
-cnt = 0
-for i in range(n):
-    chess_board[i] = 1
